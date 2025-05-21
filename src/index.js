f5d34505a39e0ab8889707ff25ab70aec349ece4
anhvuDev103 <anhvu.dev.103@gmail.com>
2025-05-21 10:31:50 +0700

    lesson 11

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { Client, IntentsBitField, ActivityType } = require("discord.js");
 const eventHandler = require("./handlers/eventHandler");
+const mongoose = require("mongoose");
 
 const client = new Client({
   intents: [
@@ -11,6 +12,15 @@ const client = new Client({
   ],
 });
 
-eventHandler(client);
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to DB");
+
+    eventHandler(client);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+})();
 
 client.login(process.env.TOKEN);
